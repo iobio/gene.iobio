764355a80d6777918f3286b5e62a890304aa8024
tonydisera <tony.disera@gmail.com>
2018-06-04 17:55:36 -0600

    #78 Make styling of navbar customizable in _variables.sass, shorten navbar

--- client/app/components/viz/Navigation.vue
+++ client/app/components/viz/Navigation.vue
@@ -7,6 +7,9 @@ aside.navigation-drawer
   z-index: 0
 
 nav.toolbar
+  -webkit-box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.13), 0px 4px 5px 0px rgba(0, 0, 0, 0.06), 0px 1px 10px 0px rgba(0,0,0,0.12)
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.13), 0px 4px 5px 0px rgba(0, 0, 0, 0.06), 0px 1px 10px 0px rgba(0,0,0,0.12)
+
   background-color: $nav-color !important
   font-weight: 300 !important
 
