a2043689dc3c8cf950c4439c9205a8e12a9a5a9e
tonydisera <tony.disera@gmail.com>
2018-07-31 20:12:27 -0600

    deploy script - upload build.js.map

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#NODE_ENV=production npm run webpack
+
 rm -rf deploy
 mkdir deploy
 
@@ -11,11 +13,12 @@ ln -s ~/work/gene.iobio.vue/client/assets ~/work/gene.iobio.vue/deploy/tmp/gene.
 ln -s ~/work/gene.iobio.vue/client/js/thirdparty ~/work/gene.iobio.vue/deploy/tmp/gene.iobio.vue/js/thirdparty
 ln -s ~/work/gene.iobio.vue/client/app/third-party ~/work/gene.iobio.vue/deploy/tmp/gene.iobio.vue/app/third-party
 ln -s ~/work/gene.iobio.vue/client/dist/build.js ~/work/gene.iobio.vue/deploy/tmp/gene.iobio.vue/dist/build.js
+ln -s ~/work/gene.iobio.vue/client/dist/build.js.map ~/work/gene.iobio.vue/deploy/tmp/gene.iobio.vue/dist/build.js.map
 ln -s ~/work/gene.iobio.vue/server/views/index.html ~/work/gene.iobio.vue/deploy/tmp/gene.iobio.vue/index.html
 
 cd deploy/tmp
 
 #zip -r  ../gene.iobio.vue.zip ./gene.iobio.vue/*
 
-#aws s3 cp ~/work/gene.iobio.vue/deploy/tmp/gene.iobio.vue/  s3://static.iobio.io/vue.gene.iobio.io/ --recursive
+aws s3 cp ~/work/gene.iobio.vue/deploy/tmp/gene.iobio.vue/  s3://static.iobio.io/vue.gene.iobio.io/ --recursive
 
