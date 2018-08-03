2cf72727719b7dd67fa2738651c61dbbdcd49b88
tonydisera <tony.disera@gmail.com>
2018-08-03 14:58:29 -0600

    New dev and prod cloudfront distributions

--- deploy.sh
+++ deploy.sh
@@ -27,10 +27,15 @@ fi
 
 # upload to cloudfront
 if [[ $1 == "prod" ]]; then
+  #aws s3 cp ./deploy/  s3://static.iobio.io/vue.gene.iobio.io/ --recursive
+  #aws cloudfront create-invalidation --distribution-id EPK0TTL11YUW --paths /
+
   echo "** Uploaded to prod s3 bucket **"
-  aws s3 cp ./deploy/  s3://static.iobio.io/vue.gene.iobio.io/ --recursive
+  aws s3 cp ./deploy/  s3://static.iobio.io/prod/gene.iobio.io/ --recursive
   echo "** Renew cloudfrount cache **"
-  aws cloudfront create-invalidation --distribution-id E1XQPXIPNUHVOH --paths /
+  aws cloudfront create-invalidation --distribution-id E331YTF25OIVP7 --paths /
+
+
 else
   echo "** Syncing to dev s3 bucket **"
   #aws s3 sync ./deploy/  s3://static.iobio.io/dev/gene.iobio.io/
