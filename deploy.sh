b5e1ae883afeb75dfb02dcac5a6e9a363d2a131f
tonydisera <tony.disera@gmail.com>
2018-09-09 19:38:52 -0600

    Change to prod.gene.iobio.io cloudfront distribution

--- deploy.sh
+++ deploy.sh
@@ -27,15 +27,13 @@ fi
 
 # upload to cloudfront
 if [[ $1 == "prod" ]]; then
-  #aws s3 cp ./deploy/  s3://static.iobio.io/vue.gene.iobio.io/ --recursive
-  #aws cloudfront create-invalidation --distribution-id EPK0TTL11YUW --paths /
 
   echo "** Uploaded to prod s3 bucket **"
-  #aws s3 cp ./deploy/  s3://static.iobio.io/prod/gene.iobio.io/ --recursive
-  aws s3 cp ./deploy/  s3://static.iobio.io/vue.gene.iobio.io/ --recursive
+  aws s3 cp ./deploy/  s3://static.iobio.io/prod/gene.iobio.io/ --recursive
+  #aws s3 cp ./deploy/  s3://static.iobio.io/vue.gene.iobio.io/ --recursive
   echo "** Renew cloudfrount cache **"
-  #aws cloudfront create-invalidation --distribution-id E331YTF25OIVP7 --paths /
-  aws cloudfront create-invalidation --distribution-id EPK0TTL11YUW --paths /
+  aws cloudfront create-invalidation --distribution-id E331YTF25OIVP7 --paths /
+  #aws cloudfront create-invalidation --distribution-id EPK0TTL11YUW --paths /
 
 
 else
