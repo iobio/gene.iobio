fee69067e7a8c8bb7f0a91434aca4e88c9d51dd4
stefinfection <sjeangeo@gmail.com>
2019-01-29 14:58:58 -0700

    fix standard in out arg for bcftools view -G

--- client/app/models/EndpointCmd.js
+++ client/app/models/EndpointCmd.js
@@ -173,7 +173,7 @@ export default class EndpointCmd {
     // TODO: if we're in sfari mode, get rid of INFO columns for each sample
     if (sfariMode === true) {
         cmd = cmd.pipe(me.IOBIO.bcftools, ['view', '-'], {ssl: me.globalApp.useSSL});
-        cmd = cmd.pipe(me.IOBIO.bcftools, ['view', '-G'], {ssl: me.globalApp.useSSL});
+        cmd = cmd.pipe(me.IOBIO.bcftools, ['view', '-G', '-'], {ssl: me.globalApp.useSSL});
         //cmd = cmd.pipe(me.IOBIO.bcftools, ['annotate', '-c', 'CHROM,POS,REF,ALT', '-'], {ssl: me.globalApp.useSSL});
         // let cutUrl = 'nv-dev-new.iobio.io/usr/bin/' + 'cut/';
         // let grepUrl = 'nv-dev-new.iobio.io/usr/bin/' + 'grep/';
