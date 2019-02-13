6ceb26ee384a49d51a41c13ca43ca02556c49bb6
tonydisera <tony.disera@gmail.com>
2019-02-13 16:17:25 -0700

    #325 Width of ranked variants card needs to be as wide as the matrix.  Also, fix sizing issue with feature matrix note.

--- client/app/components/viz/FeatureMatrixViz.vue
+++ client/app/components/viz/FeatureMatrixViz.vue
@@ -2,7 +2,12 @@
 @import ../../../assets/sass/variables
 
 #feature-matrix
+  width: max-content
+  width: -moz-max-content
 
+#feature-matrix-viz
+  width: max-content
+  width: -moz-max-content
 
   rect.cellbox
     fill: white
