9fd8985529bc47aa5231304a8f0bb0b283a499f7
tonydisera <tony.disera@gmail.com>
2019-07-02 18:09:23 -0600

    Refine save analysis dialog so that it is more obvious that we are auto-saving the analysis.  Move dialog to sit beside floating action button.

--- client/app/components/partials/SaveAnalysisPopup.vue
+++ client/app/components/partials/SaveAnalysisPopup.vue
@@ -1,47 +1,59 @@
 <style lang="sass" >
 @import ../../../assets/sass/variables
 
-.close-button
-  margin: 0px !important
-  padding: 0px !important
-  min-width: 25px !important
-  height: 25px !important
-  margin-bottom: 15px !important
-
-  .btn__content
-    padding: 0px
-    max-width: 25px
-    max-height: 25px
-
-    i.material-icons
-      font-size: 25px
-      color: $text-color
-
-
-
 .save-analysis-dialog
-  padding: 15px 15px 20px 15px
+  position: fixed
+  right: 150px
+  bottom: 0
+  z-index: 999
+
+
+.save-analysis-content
+  padding: 15px 15px 15px 15px
+
+  textarea
+    font-size: 13px !important
+  input
+    font-size: 13px !important
+
+  .close-button
+    margin: 0px !important
+    padding: 0px !important
+    min-width: 20px !important
+    height: 20px !important
+    margin-bottom: 15px !important
+
+    .btn__content
+      padding: 0px
+      max-width: 20px
+      max-height: 20px
+
+      i.material-icons
+        font-size: 20px
+        color: $text-color
 
   .layout
     padding-left: 15px
     padding-right: 15px
 
   .info-title
-    font-size: 20px
+    font-size: 14px
     color: $app-color
     margin-bottom: 15px
-  .info-description
-    font-size: 13px
+
 
 
 </style>
+
+
+
 <template>
 
-    <v-dialog  width="500" persistent v-model="showPopup" >
+    <v-dialog  content-class="save-analysis-dialog" width="400" persistent v-model="showPopup" >
 
-      <v-card class="save-analysis-dialog full-width">
+      <v-card class="save-analysis-content full-width">
         <v-card-title style="justify-content:space-between">
-          <span class="info-title">Analysis</span>
+          <span class="info-title"> {{ analysis.id ? 'Name and description of this analysis' : 'Add this analysis to Mosaic' }}</span>
           <v-btn  @click="onClose" flat class="close-button">
             <v-icon>close</v-icon>
           </v-btn>
@@ -50,14 +62,15 @@
 
 
               <v-flex xs12>
-                <v-text-field v-model="analysisName" label="Name" type="text">
+                <v-text-field hide-details="true" v-model="analysisName" label="Name" type="text">
                 </v-text-field>
               </v-flex>
 
-              <v-flex xs12>
+              <v-flex class="mt-2" xs12>
                 <v-text-field
                   multi-line
                   rows="3"
+                  hide-details="true"
                   label="Description"
                   v-model="analysisDescription"
                 >
@@ -65,8 +78,8 @@
               </v-flex>
 
 
-              <v-flex xs12>
-                <v-btn @click="onSave">Save</v-btn>
+              <v-flex class="text-xs-right mt-4" xs12>
+                <v-btn class="primary" @click="onSave">{{ analysis.id ? 'Apply' : 'Save' }}</v-btn>
                 <v-btn @click="onClose">Cancel</v-btn>
               </v-flex>
 
@@ -107,6 +120,10 @@ export default {
           this.analysisDescription = this.analysis.description;
         }
       }
+    },
+    created() {
+    },
+    mounted() {
     },
     methods: {
       onSave: function() {
