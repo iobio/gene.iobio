12da6291e0c6f10c86eadbc46d3cf47fadce945f
Will Richards <richardsw@carleton.edu>
2020-04-10 14:19:08 -0600

    updating quarantest to build from dev

--- quarantest_build.sh
+++ quarantest_build.sh
@@ -3,7 +3,7 @@
 cd /src
 # TODO: figure out how to get rid of unsafe-perm
 npm install --unsafe-perm
-./build.sh prod
+./build.sh
 # Need to use -L to follow symlinks otherwise they'll point to the wrong place
 # in the host environment
 cp -aL deploy/* /build/
