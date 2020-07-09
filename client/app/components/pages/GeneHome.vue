4793d13e5148386809b20585e2f32f12b5a093cf
Tonya Di Sera <tony.disera@gmail.com>
2020-07-09 08:43:17 -0600

    Be able to launch variant sets from clin.iobio - removed deprecated code for old workaround that used variantSets from files.

--- client/app/components/pages/GeneHome.vue
+++ client/app/components/pages/GeneHome.vue
@@ -3926,7 +3926,6 @@ export default {
 
             if (self.clinSetData.analysis.payload.variants && self.clinSetData.analysis.payload.variants.length > 0 ) {
 
-              self.variantSetCounts = {total: 0}
               self.analysis.payload.variants.forEach(function(importedVariant) {
               })
 
