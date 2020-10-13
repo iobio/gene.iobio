4329a548d8b19356f99672ed4553c806a96e843b
Will Richards <richardsw2017@gmail.com>
2020-10-13 11:50:13 -0600

    fixed bug for null selectedGene when launched from clin

--- client/app/components/pages/GeneHome.vue
+++ client/app/components/pages/GeneHome.vue
@@ -3952,7 +3952,8 @@ export default {
             }
           })
         })
-        if ((self.paramAnalysisId || self.paramVariantSetId || !self.geneClicked) && firstFlaggedVariant && !getGeneName(firstFlaggedVariant) === self.selectedGene.gene_name) {
+        console.log("self.geneClicked", self.geneClicked);
+        if ((self.paramAnalysisId || self.paramVariantSetId || !self.geneClicked) && firstFlaggedVariant &&  (!self.selectedGene || getGeneName(firstFlaggedVariant) !== self.selectedGene.gene_name)) {
           self.promiseLoadGene(getGeneName(firstFlaggedVariant))
             .then(function() {
               self.toClickVariant = firstFlaggedVariant;
