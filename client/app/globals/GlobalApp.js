9483ffb5ab4900749b2d80e3b428beb7ae76c1e6
Tonya Di Sera <tony.disera@gmail.com>
2021-03-18 20:05:20 -0600

    EMERGENCY BUG FIX. 4.4.4.4 For Mosaic variant sets, the field gene_symbol is no longer provided on the variant object. #728 Make it backward compatible to support variant sets where variant DOES have gene_symbol filled in. EMERGENCY BUG FIX gene.iobio 4.4.4.4 Fix typo

--- client/app/globals/GlobalApp.js
+++ client/app/globals/GlobalApp.js
@@ -10,7 +10,7 @@ class GlobalApp {
     this.completedTour         = "";
 
 
-    this.version               = "4.4.4.3";
+    this.version               = "4.4.4.4";
 
     this.GREEN_IOBIO           = "nv-green.iobio.io/";  // Must always stay at green to accommodate VEP service
 
