e698bb2e23a366496471aff95ff7d4cb963ff5aa
TONYA DI SERA <tony.disera@gmail.com>
2022-07-21 17:08:38 -0600

    Allow public clin.iobio to run when launched from udn mosaic.

--- client/app/components/pages/GeneHome.vue
+++ client/app/components/pages/GeneHome.vue
@@ -928,7 +928,8 @@ export default {
         "nv-prod.iobio.io":                      {iobio: "mosaic.chpc.utah.edu/gru/api/v1", batchSize: 10},
 
         "https://staging.frameshift.io":         {iobio: "backend.iobio.io",     batchSize: 10},
-        "https://mosaic.frameshift.io":          {iobio: "backend.iobio.io",     batchSize: 10}
+        "https://mosaic.frameshift.io":          {iobio: "backend.iobio.io",     batchSize: 10},
+        "https://udn.mosaic.frameshift.io":      {iobio: "backend.iobio.io", batchSize: 10}
       },
 
       sfariSource:  "https://viewer.sfari.org",
