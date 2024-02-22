b620e6f8497a49dd967d01abd69fa3146a06dc5a
TONYA DI SERA <tony.disera@gmail.com>
2024-02-21 18:48:45 -0700

    Bump up to version 4.9a

--- client/app/globals/GlobalApp.js
+++ client/app/globals/GlobalApp.js
@@ -9,7 +9,7 @@ class GlobalApp {
     this.tour                  = "";
     this.completedTour         = "";
 
-    this.version               = "4.9";
+    this.version               = "4.9a";
 
     this.GREEN_IOBIO           = "nv-green.iobio.io/";  // Must always stay at green to accommodate VEP service
 
