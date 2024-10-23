65fec5d6095684cbd082592e815840961a7ae17e
TONYA DI SERA <tony.disera@gmail.com>
2024-10-23 10:02:54 -0600

    Bump up to version 4.11.3

--- client/app/globals/GlobalApp.js
+++ client/app/globals/GlobalApp.js
@@ -9,7 +9,7 @@ class GlobalApp {
     this.tour                  = "";
     this.completedTour         = "";
 
-    this.version                = "4.11.2";
+    this.version                = "4.11.3";
 
     this.IOBIO_SERVICES         = null;
     this.HTTP_SERVICES          = null;
